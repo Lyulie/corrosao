3afbeaf199548b37ab333524048d03e987a51c7f
Rodolpho Alves <rrpa15@live.com>
2025-03-07 21:25:00 -0300

    padding

--- src/pages/components/content.tsx
+++ src/pages/components/content.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export default function Content() {
     return (
-        <main className="relative flex flex-col h-[540px] w-full bg-sea">
+        <main className="relative flex flex-col h-fit w-full bg-sea">
 
             <div className="absolute top-0 left-0 w-full h-20">
                 <Image src="/curva.svg" alt="Curva Topo" layout="fill" objectFit="cover" />
             </div>
     
-            <div className="flex flex-grow justify-center items-center flex-col text-center z-10">
+            <div className="flex flex-grow justify-center items-center flex-col text-center z-10 py-20">
                 <h1 className="text-6xl text-black">Corrosão:</h1>
                 <p className="text-4xl">Introdução e Avaliação</p>
                 <p className="text-4xl mt-12">2024</p>
