818d70d5b8479515f047eebbcd94c5809cbd2040
Rodolpho Alves <rrpa15@live.com>
2025-03-08 21:09:50 -0300

    upd

--- src/pages/components/content.tsx
+++ src/pages/components/content.tsx
@@ -4,20 +4,30 @@ import Image from 'next/image';
 export default function Content() {
     return (
         <main className="relative flex flex-col h-fit w-full bg-sea">
+          <div className="absolute top-0 left-0 w-full h-28 overflow-visible">
+            <Image 
+              src="/curva-top.svg" 
+              alt="Curva superior" 
+              layout="fill"
+              objectFit="fill"
+              priority
+            />
+          </div>
+          
+          <div className="flex flex-grow justify-center items-center flex-col text-center z-10 py-20">
+            <h1 className="text-6xl text-black">Corrosão:</h1>
+            <p className="text-4xl">Introdução e Avaliação</p>
+            <p className="text-4xl mt-12">2024</p>
+          </div>
 
-            <div className="absolute top-0 left-0 w-full h-20">
-                <Image src="/curva.svg" alt="Curva Topo" layout="fill" objectFit="cover" />
-            </div>
-    
-            <div className="flex flex-grow justify-center items-center flex-col text-center z-10 py-20">
-                <h1 className="text-6xl text-black">Corrosão:</h1>
-                <p className="text-4xl">Introdução e Avaliação</p>
-                <p className="text-4xl mt-12">2024</p>
-            </div>
-
-            <div className="absolute bottom-0 left-0 w-full h-20 rotate-180">
-                <Image src="/curva.svg" alt="Curva Fundo" layout="fill" objectFit="cover" />
-            </div>
+          <div className="absolute bottom-0 left-0 w-full h-28 overflow-visible">
+            <Image 
+              src="/curva-bottom.svg" 
+              alt="Curva inferior" 
+              layout="fill"
+              objectFit="fill"
+            />
+          </div>
         </main>
-    );
+      );       
 }
